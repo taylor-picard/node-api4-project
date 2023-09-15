5366157b22f39c1100b6429d00764715186693d4
Taylor Picard <taylorkpicard1998@gmail.com>
2023-09-14 20:25:07 -0500

    API requests complete

--- index.js
+++ index.js
@@ -9,8 +9,25 @@ const PORT = process.env.PORT || 9000;
 server.use(express.json());
 server.use(cors());
 
-server.get('/api/hello', (req, res) => {
-  res.json({message: 'api working'});
+const users= [{username: 'user1', password: 'pass1'},
+  {username: 'user2', password: 'pass2'},
+  {username: 'user3', password: 'pass3'}];
+
+server.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+server.post('/api/register', (req, res)=> {
+  const {username, password}= req.body;
+  const newUser = {username: username, password: password};
+  res.status(201).json(newUser);
+});
+
+server.post('/api/login', (req, res)=> {
+  const {username, password}= req.body;
+  res.status(201).json({
+    message: `Welcome, ${username}!`,
+  });
 });
 
 server.use('*', (req, res) => {
